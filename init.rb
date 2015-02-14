49818cd882818906c8c2a6b6d9df7f62ef80bd23
justi <justine84@gmail.com>
2015-02-14 19:43:36 +0100

    Version bump to 0.1.3

--- init.rb
+++ init.rb
@@ -7,6 +7,6 @@ Redmine::Plugin.register :redmine_get_badges do
   name 'Redmine Get Badges plugin'
   author 'Justyna Wojtczak'
   description 'A plugin for managing getbadges functionality'
-  version '0.1.2'
+  version '0.1.3'
 end
 
