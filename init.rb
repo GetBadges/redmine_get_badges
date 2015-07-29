98c0fc87dac33385b0b07ddffcb54f027184a8a1
Justyna Wojtczak <justine84@gmail.com>
2015-07-29 22:37:24 +0200

    Fix desc

--- init.rb
+++ init.rb
@@ -4,9 +4,9 @@ require 'redmine_get_badges/patches/setting_patch'
 
 Redmine::Plugin.register :redmine_get_badges do
   settings default: {'empty' => true}, partial: 'redmine_get_badges/settings'
-  name 'Redmine Get Badges plugin'
+  name 'Redmine GetBadges plugin'
   author 'Justyna Wojtczak'
-  description 'A plugin for managing getbadges functionality'
+  description 'A plugin for managing GetBadges functionality'
   version '0.1.4'
 end
 
