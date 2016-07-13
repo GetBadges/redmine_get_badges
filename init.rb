a6d7710891cbd331ac6dfa128140bc6af7e4e7e1
Krzysztof Hasiński <krzysztof.hasinski@gmail.com>
2016-07-13 21:44:56 +0200

    Clean up

--- init.rb
+++ init.rb
@@ -3,10 +3,10 @@ require 'redmine_get_badges/patches/project_patch'
 require 'redmine_get_badges/patches/setting_patch'
 
 Redmine::Plugin.register :redmine_get_badges do
-  settings default: {'empty' => true}, partial: 'redmine_get_badges/settings'
+  settings default: { 'empty' => true }, partial: 'redmine_get_badges/settings'
   name 'Redmine GetBadges plugin'
   author 'Justyna Wojtczak'
-  description 'A plugin for managing GetBadges functionality'
-  version '0.1.5'
+  description 'A plugin for managing GetBadges integration'
+  version '0.1.6'
 end
 
